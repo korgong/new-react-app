6ddac6aa05c879819cce42854ba577178ad2b5ac
korgong <cquptgk@gmail.com>
2023-06-14 17:04:43 +0800

    add config for prettier

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import Playground from "./Playground";
 
 // want to write a list rendered by data
 function App() {
-  return (
-    <div className="App">
-      <Playground />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Playground />
+        </div>
+    );
 }
 
 export default App;
