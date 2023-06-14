179fea806e3ec55bda0e1c0849dcbdb3e255b2e4
korgong <cquptgk@gmail.com>
2023-06-14 16:55:39 +0800

    add a list page

---
+++ src/Playground.js
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+import './Playground.css';
+
+// want to write a list rendered by data
+// now i want to scroll the list
+function Playground() {
+    const initialArr = new Array({length: 20});
+  const [list, setList] = useState(initialArr);
+  return (
+      <div className="list">
+        <ul className='list-container'>
+          {list.map((_item, index) => (
+            <li key={index}>item {index}</li>
+          ))}
+        </ul>
+      </div>
+  );
+}
+
+export default Playground;
